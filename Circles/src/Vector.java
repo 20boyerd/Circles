d16f3ece02e2dd0f4af9db0200106d6982598d47
Derek Boyer <derekboyer33@outlook.com>
2017-01-29 00:51:53 -0500

    Update Vector.java

--- Circles/src/Vector.java
+++ Circles/src/Vector.java
@@ -1,4 +1,19 @@
+public class Vectors {
+	private double x;
+	private double y;
+	
+	public Vectors(){
+		x = 0;
+		y = 0;
+	}
+	
+	public Vectors(double x, double y){
+		x = this.x;
+		y = this.y;
+	}
 
-public interface Vector {
-
+	public Vectors(double t){
+		x = Math.cos(t);
+		y = Math.sin(t);
+	}
 }
